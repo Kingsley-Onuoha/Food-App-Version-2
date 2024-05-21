a291d803e0ffe3a0fe83029acf1355931002816b
Kingsley Onuoha <onuohakingsley521@gmail.com>
2024-05-21 11:39:27 +0100

    api key change

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyDeKZawuFWGIci0NwGomvvFH1cqMIJuFec",
   authDomain: "swiggy-food-f842d.firebaseapp.com",
   projectId: "swiggy-food-f842d",
   storageBucket: "swiggy-food-f842d.appspot.com",
