dfda3b772713aec43ea36c916f514e5c4077b98f
Kingsley Onuoha <onuohakingsley521@gmail.com>
2024-05-21 11:54:34 +0100

    cors proxy change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 export const CLOUDINARYIMAGEID_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-export const MENU_API_DESKTOP = "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.969539&lng=72.819329&restaurantId="
+export const MENU_API_DESKTOP =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.969539&lng=72.819329&restaurantId=";
 
-export const MENU_API_MOBILE = "https://corsproxy.io/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.969539&lng=72.819329&restaurantId="
+export const MENU_API_MOBILE =
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.969539&lng=72.819329&restaurantId=";
